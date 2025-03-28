9515d8a90931298c8747b805eaf6b00c3599219c
Uttkarsh Khanke <uttkarsh.khanke@gmail.com>
2025-03-28 22:14:39 +0530

    added Basic_Python_Syntax

---
+++ 3 Scripting for DevOps (Python, Ruby, Golang)/3.2 Python Basics (Syntax & Fundamentals)/Basic_Python_Syntax.py
@@ -0,0 +1,22 @@
+# Print statement
+print("Hello, DevOps!")
+
+# Variables
+name = "DevOps Engineer"
+age = 25
+
+# Conditions
+if age > 18:
+    print("You are eligible!")
+else:
+    print("Not eligible!")
+
+# Loops
+for i in range(5):
+    print(i)
+
+# Functions
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("DevOps"))
